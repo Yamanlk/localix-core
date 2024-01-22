080335156e4aa0214c45c01ae5266795f84674be
yaman lakis <yamanlakis@gmail.com>
2024-01-22 13:09:38 +0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center">@localix/core</h1>
+
+<p align="center">Seamlessly localize your typescript applications. <p align="center">
+<a href="https://www.npmjs.com/~localix" target="_blank"><img src="https://img.shields.io/npm/v/@localix/core.svg" alt="NPM Version" /></a>
+<a href="https://www.npmjs.com/~localix" target="_blank"><img src="https://img.shields.io/npm/l/@localix/core.svg" alt="Package License" /></a>
+<a href="https://www.npmjs.com/~localix" target="_blank"><img src="https://img.shields.io/npm/dm/@localix/core.svg" alt="NPM Downloads" /></a>
+</p>
+
+## Description
+
+"@localix/core" provides localized template tag."
+
+Please read the docs for more information [Localix Documentation](https://localix.gitbook.io/documentation).
---
+++ src/index.ts
@@ -0,0 +1,74 @@
+import { readFileSync } from "fs";
+
+/**
+ * @description localization map
+ */
+const translations: Record<string, Record<string, string>> = {};
+let _options: LocalizationOptions;
+
+/**
+ *
+ * @param strings
+ * @param args
+ * @returns object containing locale as key and localized string as value
+ */
+export function localize(
+  strings: TemplateStringsArray,
+  ...args: any[]
+): Record<string, string> {
+  const id = strings.raw
+    .map((string, index) =>
+      index === strings.raw.length - 1 ? string : string + `$${index + 1}`
+    )
+    .join("");
+
+  const translation = translations[id];
+
+  if (!translation) {
+    return {
+      [_options.defaultLocale]: String.raw(strings, args),
+    };
+  }
+
+  return Object.keys(translation).reduce((acc, locale) => {
+    let interpolated = translation[locale];
+
+    args.forEach((arg, index) => {
+      interpolated = interpolated.replace(
+        new RegExp(`\\$${index + 1}`, "g"),
+        arg
+      );
+    });
+
+    return {
+      ...acc,
+      [locale]: interpolated,
+    };
+  }, {});
+}
+
+export interface LocalizationOptions {
+  /**
+   * @description default locale when translations not found.
+   */
+  defaultLocale: string;
+  /**
+   * @description json localization file path.
+   */
+  localizationFile: string;
+}
+
+/**
+ *
+ * @param options
+ * @description initialize localization, reads translation file and loads it to memory.
+ */
+export function initLocalization(options: LocalizationOptions) {
+  const localizationFile = readFileSync(options.localizationFile, {
+    encoding: "utf-8",
+  });
+
+  Object.assign(translations, JSON.parse(localizationFile));
+
+  _options = options;
+}
